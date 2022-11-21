a2e0e12d23dfd4393522b5b094842f584379ad2f
David Isaksson <davidisaksson93@gmail.com>
2022-11-21 22:53:54 +0100

    Fix error handling for `mkdir -p` command

--- mc-util.sh
+++ mc-util.sh
@@ -50,7 +50,7 @@ create_backup()
     local timestamp
     timestamp=$(date "+%F_%T" | tr ":" "_")
     local backup_filename="backup_$timestamp.tar.gz"
-    if [ "$(mkdir -p "$BACKUP_DIR")" ]; then
+    if ! mkdir -p "$BACKUP_DIR"; then
         log "Failed to create $BACKUP_DIR"
         exit 1
     fi
